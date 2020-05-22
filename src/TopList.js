05fbb825332ebc06affef772ca33409771ee8afd
SkaB <babeanusorn@gmail.com>
2020-05-22 23:44:36 +0700

    เมื่อไรจะเสร็จ

--- src/TopList.js
+++ src/TopList.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import img1 from './img/1.jpg';
-    const TopList = () => {
-        return (
-            <>  
-                <h1>TOP 10 ร้านอาหารดีเด่น ประจำสัปดาห์</h1>
-                <div class="gallery">
-                    <a href={img1}>
-                        <img src={img1} alt="res1" width="100" height="500"></img>
-                    </a>
-                    <div class="desc">Add a description of the image here</div>
-                </div>
+const TopList = () => {
+    return (
+        <>
+            <h1>TOP 10 ร้านอาหารดีเด่น ประจำสัปดาห์</h1>
+            <div class="galleries">
 
                 <div class="gallery">
                     <a href={img1}>
@@ -38,8 +33,17 @@ import img1 from './img/1.jpg';
                     </a>
                     <div class="desc">Add a description of the image here</div>
                 </div>
-            </>
-        )
-    }
-    
+
+                <div class="gallery">
+                    <a href={img1}>
+                        <img src={img1} alt="res1" width="100" height="500"></img>
+                    </a>
+                    <div class="desc">Add a description of the image here</div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
 export default TopList; 
