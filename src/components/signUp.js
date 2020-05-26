10350c17bbd6b72af633a7d35c31fbc185b58f9e
SkaB <babeanusorn@gmail.com>
2020-05-26 11:19:15 +0700

    fix signup

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,77 +1,82 @@
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
 
-const SignUp = ()=> {
+const SignUp = () => {
 
-    // const test = {
-    //     username:"dasdasdwr",
-	//     email:"patrapsde@gmail.com",
-	//     password:"12345678"
-    // }
-
-    const initialFormData = Object.freeze({
+    const initialFormData = {
         username: "",
         email: "",
         password: ""
-    });
+    };
 
+    const [uname, setUname] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
     const [formData, updateFormData] = useState(initialFormData);
 
-    const handleChange = (e) => {
-        updateFormData({
-            ...formData,
-      
-            // Trimming any whitespace
-            [e.target.name]: e.target.value.trim()
-        });
+    const unameChange = (e) => {
+        setUname(e.target.value);
     };
-    
+
+    const emailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const passChange = (e) => {
+        setPass(e.target.value);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        updateFormData({
+            username: uname,
+            email: email,
+            password: pass
+        })
         console.log(formData);
         // ... submit to API or something
 
         fetch('/api/user/signUp', {
-            method: 'POST', 
-            body: JSON.stringify(formData), 
+            method: 'POST',
+            body: JSON.stringify(formData),
             headers: new Headers({
-            'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             })
         }).then(res => res.json())
-        .catch(error => {
-            console.error('Error:', error);
-        })
-        .then(res => {
-            console.log('Success:', res);
-        })               
-    
+            .catch(error => {
+                console.error('Error:', error);
+            })
+            .then(res => {
+                console.log('Success:', res);
+            })
+
     };
-    
-        return (
-            <form class="SignUp"onSubmit={handleSubmit}>
-                <h3>สมัครสมาชิก</h3>
 
-                <div className="form-group">
-                    <label>User name</label>
-                    <input type="text" className="form-control" placeholder="User name" onChange={handleChange}/>
-                </div>
+    return (
+        <form class="SignUp" onSubmit={handleSubmit}>
+            <h3>สมัครสมาชิก</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email"onChange={handleChange} />
-                </div>
+            <div className="form-group">
+                <label>User name</label>
+                <input type="text" className="form-control" placeholder="User name" onChange={unameChange} />
+            </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" onChange={handleChange}/>
-                </div>
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" onChange={emailChange} />
+            </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button><br></br>
-                <p className="forgot-password text-right">
-                    เคยสมัครแล้ว <a href='/signIn'>เข้าสู่ระบบ?</a>
-                </p>
-            </form>
-        )
-    
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" onChange={passChange} />
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block">Sign Up</button><br></br>
+            <p className="forgot-password text-right">
+                เคยสมัครแล้ว <a href='/signIn'>เข้าสู่ระบบ?</a>
+            </p>
+        </form>
+    )
+
 }
 
 export default SignUp;
